9f42262400915b61548118b8ee071323d25ca3a3
aabhishek <aabhishekgumme9011@gmail.com>
2024-04-08 00:24:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import StudentCrud from './components/StudentCrud';
+
+function App() {
+  return (
+    <div>
+      <StudentCrud/>
+    </div>
+  );
+}
+
+export default App;
